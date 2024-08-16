d2e274cef76cae186213b013d9289bb6feb0efd7
adriGr52 <115507670+adriGr52@users.noreply.github.com>
2024-08-16 14:51:02 -0500

    Update app_google_sheets.py

--- app_google_sheets.py
+++ app_google_sheets.py
@@ -1,12 +1,11 @@
-
 import streamlit as st
 import pandas as pd
 
-# URL de la hoja de Google Sheets (con enlace compartido)
-sheet_url = 'https://docs.google.com/spreadsheets/d/1usXpjL3KAR3w5MwKYa1NZsz4WJtInJ6W/edit?usp=drive_link&ouid=109358120796349146990&rtpof=true&sd=true'
+# ID del documento de Google Sheets
+sheet_id = '1usXpjL3KAR3w5MwKYa1NZsz4WJtInJ6W'
 
-# Convertir la URL para obtener el formato adecuado para Pandas
-csv_export_url = sheet_url.replace('/edit?usp=drive_link&ouid=109358120796349146990&rtpof=true&sd=true', '/export?format=csv')
+# Formar la URL de exportación
+csv_export_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv'
 
 # Leer los datos directamente desde Google Sheets
 df = pd.read_csv(csv_export_url)
